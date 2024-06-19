d357b2824655d0a1047a001e14ea6b76782be44c
Yash Yadav <yashyadav.sr@gmail.com>
2024-06-19 15:22:19 +0400

    feat: tried to add tab capture

--- src/background.js
+++ src/background.js
@@ -1,23 +1,10 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'start') {
-      chrome.tabCapture.capture({audio: true}, function(stream) {
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-          const tabId = tabs[0].id;
-          chrome.scripting.executeScript({
-            target: {tabId: tabId},
-            func: processAudioStream,
-            args: [stream]
-          });
-        });
-      });
-    }
-  });
-  
-  function processAudioStream(stream) {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const source = audioContext.createMediaStreamSource(stream);
-  
-    // Placeholder for actual audio processing logic
-    source.connect(audioContext.destination);
-  }
-  
+// In your service worker (background.js in your case)
+chrome.action.onClicked.addListener((tab) => {
+    chrome.windows.create({
+        url: chrome.runtime.getURL("popup/popup.html"),
+        type: "popup",
+        focused: true,
+        width: 400,
+        height: 600,
+    });
+});
